f938aaebe13efab29975d57c24594022a9fb6cbc
maimuna99 <73959703+maimuna99@users.noreply.github.com>
2022-11-23 10:21:29 +0400

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <script setup>
-import HelloWorld from '../components/HelloWorld.vue';
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+//import Photo from './components/Photo.vue'
 </script>
 
 <template>
+  <div>
+ 
+  </div>
   <Suspense>
-    <HelloWorld />
+    <HelloWorld class="async-component" />
+    <template #fallback>
+      <SimpleLoading class="async-component" />
+    </template>
   </Suspense>
+  
 </template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,27 @@
 <script setup>
-import { ref } from 'vue';
-const response = ref({});
-
-// adding delay
-await new Promise((resolve) => {
-  setTimeout(() => {
-    resolve();
-  }, 1000);
-});
-
-try {
-  const res = await fetch('https://yesno.wtf/api');
-  console.log(res);
-  response.value = await res.json();
-} catch (error) {
-  response.value = {
-    answer: 'Error! Could not reach the API. ' + error,
-    image: '',
-  };
-}
+	import { ref, onErrorCaptured } from 'vue'
+	const response = ref({})
+  // adding delay
+  await new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, 1000)
+  })
+  onErrorCaptured((err) => {
+    // error processing
+  })
+  const res = await fetch('https://jsonplaceholder.typicode.com/photos/' + Math.floor(Math.random() * 10) + 1);
+  console.log(res)
+  if (!res.ok) {
+    throw new Error()
+  }
+  response.value = (await res.json())
 </script>
 
 <template>
-  <p>{{ response.answer }}</p>
-  <img :src="response.image" />
+  <div>
+    <p>{{ response.albumId }}</p>
+    <p>{{ response.title }}</p>
+    <img :src="response.thumbnailUrl" />
+  </div>
 </template>
